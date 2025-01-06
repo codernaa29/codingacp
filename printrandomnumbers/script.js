52fc239848df3a483e0e9be2f5f3ec05d5b992ce
codernaa29 <01717360938hawa@gmail.com>
2025-01-06 06:43:59 -0800

    5th commit

--- printrandomnumbers/script.js
+++ printrandomnumbers/script.js
@@ -2,7 +2,13 @@ var i; // i is undefined
 var text = " ";
 {
     let i = 0; // assigning a value for i only within the block 
-    
+    while (i<10){
+        text += "The values inside the block: "+ i + "</br>"; i++;
+        }
 
+    document.getElementById("result").innerHTML = text;
 }
+
+document.write("The value of i outside the book is: "+i);
+
 
