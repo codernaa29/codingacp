fef4e6c6e313d4eb77606bfbb5ce15853b7469c7
codernaa29 <01717360938hawa@gmail.com>
2025-05-30 00:54:57 +0600

    17th commit

--- Stopchangingnumber/script.js
+++ Stopchangingnumber/script.js
@@ -1,18 +1,23 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var changing = false;
+var isRunning = true;
 var interval;
+var clickCount = 0;
 
-function changeNumber() {
+function change() {
     var randomIndex = Math.floor(Math.random() * numbers.length);
-    document.getElementById("number").innerHTML = numbers[randomIndex];
+    document.getElementById("number").innerText = numbers[randomIndex];
 }
 
-function toggleNumber() {
-    if (changing == false) {
-        interval = setInterval(changeNumber, 100);
-        changing = true;
+function startStop() {
+    clickCount++;
+
+    if (isRunning) {
+        // d: If true, start changing and set to false
+        interval = setInterval(change, 100);
+        isRunning = false;
     } else {
+        // e: If false, stop changing and set to true
         clearInterval(interval);
-        changing = false;
+        isRunning = true;
     }
 }
